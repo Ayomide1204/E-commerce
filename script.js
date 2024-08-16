a06833ee2290b2028bc0a98b2446a387520f8b92
ayomide <ahmedayomide0412@gmail.com>
2024-08-16 16:23:21 +0100

    Friday's commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+    var menuitems=document.getElementByid("menuitems");
+    menuitems.style.maxHeight="0px";
+
+    function menuggle(){
+        if(menuitems.style.maxHeight="0px")
+    {
+        menuitems.style.maxHeight="200px";
+    }
+    else
+        {
+            menuitems.style.maxHeight="10px";
+        }
+    };
